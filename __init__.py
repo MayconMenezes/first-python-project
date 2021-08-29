a52964538de7aef199c0ce376303c06b72f5529d
MayconMenezes <ap_maycon@hotmail.com>
2021-08-29 15:54:16 -0300

    feat: add author info

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__author__      = "Maycon Menezes"
+__credits__     = ["Maycon Menezes"]
+__license__     = "MIT"
+__version__     = "0.0"
+__maintainer__  = "Maycon Menezes"
+__email__       = "ap_maycon@hotmail.com"
+__status__      = "Studing"
