9fed49fb942d39ace9069c7c94a233a4af78f6b1
MayconMenezes <ap_maycon@hotmail.com>
2021-08-29 16:29:32 -0300

    refactor: init app on list person

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 from pyforms import start_app
-from views.welcome.index import WelcomeView
+from views.person.list import PersonListView
 
 sys.path.append('./')
 if __name__ == "__main__":
-  start_app(WelcomeView)
+  start_app(PersonListView)
